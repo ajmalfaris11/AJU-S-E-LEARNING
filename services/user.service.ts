2ae8dd731cdbdb49099885d0b2249090c003ed32
Ajmal Faris <ajmalfaris.me@gmail.com>
2024-12-09 04:05:58 +0530

    Optimize user retrieval by using Redis cache before fetching from the database

--- services/user.service.ts
+++ services/user.service.ts
@@ -1,14 +1,23 @@
-import { Response } from "express";
-import userModel from "../models/user.model";
-
 // Function to get user details by their ID
+// This updated function first checks if the user data is available in the Redis cache using the user's ID.
+// If the user data is found in Redis, it is parsed and sent in the response.
+// If the user data is not found in Redis, no further action is taken in this code snippet, but it can be extended to fetch from the database.
+
+import { Response } from "express";
+import { redis } from "../utils/redis";
+
 export const getUserById = async (id: string, res: Response) => {
-    // Fetch the user from the database using the provided ID
-    const user = await userModel.findById(id);
-    
-    // Respond with the fetched user details in JSON format
-    res.status(201).json({
-        success: true,
-        user,
-    });
+    // Attempt to get the user data from Redis cache
+    const userJson = await redis.get(id);
+
+    // If user data is found in the cache
+    if (userJson) {
+        // Parse the user JSON and send it in the response
+        const user = JSON.parse(userJson);
+        res.status(201).json({
+            success: true,
+            user,
+        });
+    }
 };
+
