488011c9a671bf2e0e412c5f526da345f748f11b
Ajmal Faris <ajmalfaris.me@gmail.com>
2024-12-09 01:47:14 +0530

    Created /refresh route in userRouter to handle access token refresh requests.

--- routes/user.route.ts
+++ routes/user.route.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { activateUser, loginUser, logoutUser, registrationUser } from '../controllers/user.controller';
+import { activateUser, loginUser, logoutUser, registrationUser, updateAccessToken } from '../controllers/user.controller';
 import { isAuthenticated } from '../middleware/auth';
 const userRouter = express.Router();
 
@@ -11,5 +11,6 @@ userRouter.post('/login', loginUser);
 
 userRouter.get('/logout', isAuthenticated, logoutUser);
 
+userRouter.get('/refresh', updateAccessToken);
 
 export default userRouter;
