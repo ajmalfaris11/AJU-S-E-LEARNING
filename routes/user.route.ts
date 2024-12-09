2a0039199eb50cdc8a299c57bf2ed0a6875defed
Ajmal Faris <ajmalfaris.me@gmail.com>
2024-12-09 06:28:58 +0530

    feat: Add route for updating user password
    - Added PUT route '/update-user-password' for updating the user's password.
    - Applied `isAuthenticated` middleware to ensure only authenticated users can update their password.
    - Linked the route to the `updatePassword` controller for password update functionality.

--- routes/user.route.ts
+++ routes/user.route.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { activateUser, getUserInfo, loginUser, logoutUser, registrationUser, socialAuth, updateAccessToken, updateUserInfo } from '../controllers/user.controller';
+import { activateUser, getUserInfo, loginUser, logoutUser, registrationUser, socialAuth, updateAccessToken, updatePassword, updateUserInfo } from '../controllers/user.controller';
 import { isAuthenticated } from '../middleware/auth';
 const userRouter = express.Router();
 
@@ -18,5 +18,8 @@ userRouter.get('/me', isAuthenticated, getUserInfo);
 userRouter.post('/social-auth', socialAuth);
 
 userRouter.put('/update-user-info',isAuthenticated, updateUserInfo);
+
+userRouter.put('/update-user-password',isAuthenticated, updatePassword);
+
 
 export default userRouter;
