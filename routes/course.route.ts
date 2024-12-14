115cee4341892f2dcacf69a3cbb753b754376869
Ajmal Faris <ajmalfaris.me@gmail.com>
2024-12-15 03:31:18 +0530

    chore: Add route for fetching a single course by ID

--- routes/course.route.ts
+++ routes/course.route.ts
@@ -1,9 +1,10 @@
 import express from 'express';
-import { editCourse, uploadCourse } from '../controllers/course.controller';
+import { editCourse, getSingleCourse, uploadCourse } from '../controllers/course.controller';
 import { authorizedRoles, isAuthenticated } from '../middleware/auth';
 const courseRouter = express.Router();
 
 courseRouter.post("/create-course", isAuthenticated, authorizedRoles("admin"), uploadCourse);
 courseRouter.post("/edit-course/:id", isAuthenticated, authorizedRoles("admin"), editCourse);
+courseRouter.get("/get-course/:id", getSingleCourse);
 
 export default courseRouter;
