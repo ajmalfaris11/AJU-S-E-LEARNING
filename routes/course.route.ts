e25522ebec79e03fa105147d32b0f59eea073357
Ajmal Faris <ajmalfaris.me@gmail.com>
2024-12-15 03:46:50 +0530

    chore: Add route for fetching all courses

--- routes/course.route.ts
+++ routes/course.route.ts
@@ -1,10 +1,12 @@
 import express from 'express';
-import { editCourse, getSingleCourse, uploadCourse } from '../controllers/course.controller';
+import { editCourse, getAllCourse, getSingleCourse, uploadCourse } from '../controllers/course.controller';
 import { authorizedRoles, isAuthenticated } from '../middleware/auth';
 const courseRouter = express.Router();
 
 courseRouter.post("/create-course", isAuthenticated, authorizedRoles("admin"), uploadCourse);
 courseRouter.post("/edit-course/:id", isAuthenticated, authorizedRoles("admin"), editCourse);
 courseRouter.get("/get-course/:id", getSingleCourse);
+courseRouter.get("/get-courses", getAllCourse);
+
 
 export default courseRouter;
