a55473b042e9eb21247081a86b9b1c8168af015a
Ajmal Faris <ajmalfaris.me@gmail.com>
2024-12-16 01:09:19 +0530

    feat: add route to fetch course content for authenticated users
    - Created a GET route `/get-course-content/:id` to retrieve course content.
    - Integrated authentication middleware `isAuthenticated` to ensure only logged-in users can access the route.
    - Linked the `getCourseByUser` controller for handling course content retrieval logic.

--- routes/course.route.ts
+++ routes/course.route.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { editCourse, getAllCourse, getSingleCourse, uploadCourse } from '../controllers/course.controller';
+import { editCourse, getAllCourse, getCourseByUser, getSingleCourse, uploadCourse } from '../controllers/course.controller';
 import { authorizedRoles, isAuthenticated } from '../middleware/auth';
 const courseRouter = express.Router();
 
@@ -7,6 +7,8 @@ courseRouter.post("/create-course", isAuthenticated, authorizedRoles("admin"), u
 courseRouter.post("/edit-course/:id", isAuthenticated, authorizedRoles("admin"), editCourse);
 courseRouter.get("/get-course/:id", getSingleCourse);
 courseRouter.get("/get-courses", getAllCourse);
+
+courseRouter.get("/get-course-content/:id",isAuthenticated, getCourseByUser);
 
 
 export default courseRouter;
