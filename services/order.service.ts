f3ebf250007436dfb30ddc29f59ab6971915ebeb
Ajmal Faris <ajmalfaris.me@gmail.com>
2024-12-17 04:57:05 +0530

    updated the order create service function

--- services/order.service.ts
+++ services/order.service.ts
@@ -3,11 +3,13 @@ import { CatchAsyncError } from "../middleware/catchAsyncErrors";
 import OrderModel from "../models/order.model";
 
 // Controller to create a new order
-export const newOrder = CatchAsyncError(async (data: any, res: Response, next: NextFunction) => {
+export const newOrder = CatchAsyncError(async (data:any, res: Response, next: NextFunction) => {
 
     // Create a new order document in the database
     const order = await OrderModel.create(data); 
 
-    // Pass the created order to the next middleware or handler
-    next(order);
+    res.status(200).json({
+        success:true,
+        order,
+    })
 });
