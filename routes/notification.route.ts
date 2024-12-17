27d87f8049a8ef71d898630fbc789417c57c2eee
Ajmal Faris <ajmalfaris.me@gmail.com>
2024-12-17 05:48:46 +0530

    feat: Add route for updating notification status
    - Created PUT route `/update-notification/:id` to update the status of a notification to "read" for admin users.
    - Integrated `isAuthenticated` and `authorizedRoles("admin")` middleware to ensure only authenticated admin users can update notification status.
    - Tested the route with Postman and received a successful response.

--- routes/notification.route.ts
+++ routes/notification.route.ts
@@ -1,8 +1,9 @@
 import express from 'express';
 import { authorizedRoles, isAuthenticated } from '../middleware/auth';
-import { getNotifications } from '../controllers/notification.controller';
+import { getNotifications, updateNotication } from '../controllers/notification.controller';
 const notificationRoute = express.Router();
 
 notificationRoute.get("/get-all-notifications", isAuthenticated, authorizedRoles("admin"), getNotifications)
+notificationRoute.put("/update-notification/:id", isAuthenticated, authorizedRoles("admin"), updateNotication)
 
 export default notificationRoute;
