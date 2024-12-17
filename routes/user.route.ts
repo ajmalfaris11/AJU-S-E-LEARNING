cff9cf2c03e5e221eb9e87aa9fd9fa4537bd8446
Ajmal Faris <ajmalfaris.me@gmail.com>
2024-12-17 07:39:34 +0530

    feat: Add route to get all users (admin only)
    - Added a GET route `/get-users` to fetch all users, accessible only by admin users.
    - Applied authentication (`isAuthenticated`) and authorization (`authorizedRoles("admin")`) middleware to restrict access.
    - Tested the route with Postman and confirmed successful response with status 200.

--- routes/user.route.ts
+++ routes/user.route.ts
@@ -1,6 +1,6 @@
 import express from 'express';
-import { activateUser, getUserInfo, loginUser, logoutUser, registrationUser, socialAuth, updateAccessToken, updatePassword, updateProfilePicture, updateUserInfo } from '../controllers/user.controller';
-import { isAuthenticated } from '../middleware/auth';
+import { activateUser, getAllUsers, getUserInfo, loginUser, logoutUser, registrationUser, socialAuth, updateAccessToken, updatePassword, updateProfilePicture, updateUserInfo } from '../controllers/user.controller';
+import { authorizedRoles, isAuthenticated } from '../middleware/auth';
 const userRouter = express.Router();
 
 userRouter.post('/registration', registrationUser);
@@ -22,6 +22,9 @@ userRouter.put('/update-user-info',isAuthenticated, updateUserInfo);
 userRouter.put('/update-user-password',isAuthenticated, updatePassword);
 
 userRouter.put('/update-user-avatar',isAuthenticated, updateProfilePicture);
+
+userRouter.get("/get-users",isAuthenticated,authorizedRoles("admin"), getAllUsers);
+
 
 
 export default userRouter;
