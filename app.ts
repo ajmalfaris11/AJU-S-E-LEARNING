b25c8bcb6a5941a8cef42557666369fa16dfe292
Ajmal Faris <ajmalfaris.me@gmail.com>
2024-12-18 05:08:33 +0530

    - Added routes for fetching analytics data:   - `/get-users-analytics` - Fetch user analytics for the last 12 months   - `/get-courses-analytics` - Fetch course analytics for the last 12 months   - `/get-orders-analytics` - Fetch order analytics for the last 12 months - Implemented authentication and authorization middleware to ensure only admins can access these routes - Tested all routes with Postman and confirmed successful responses

--- app.ts
+++ app.ts
@@ -11,6 +11,8 @@ import userRouter from "./routes/user.route";
 import courseRouter from "./routes/course.route";
 import orderRouter from "./routes/order.route";
 import notificationRoute from "./routes/notification.route";
+import analyticsRouter from "./routes/analytics.route";
+
 
 
 
@@ -30,6 +32,7 @@ app.use("/api/v1/", userRouter);
 app.use("/api/v1/", courseRouter);
 app.use("/api/v1/", orderRouter);
 app.use("/api/v1/", notificationRoute);
+app.use("/api/v1/", analyticsRouter);
 
 
 
