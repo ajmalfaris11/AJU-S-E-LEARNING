811fb51c4fc401dc25fa79f2a0cb4ab597ff164b
Ajmal Faris <ajmalfaris.me@gmail.com>
2024-12-17 05:43:01 +0530

    feat: Implement update notification status functionality for admin
    - Added controller to update the notification status to "read" when accessed by an admin user.
    - If notification is found, its status is updated and saved.
    - After updating, the latest notifications are fetched and returned to the user.
    - Error handling in place for missing notification and general errors.

--- controllers/notification.controller.ts
+++ controllers/notification.controller.ts
@@ -20,3 +20,38 @@ export const getNotifications = CatchAsyncError(async (req: Request, res: Respon
         return next(new ErrorHandler(error.message, 500));
     }
 });
+
+
+// Controller to update the notification status to 'read' (only for admin)
+export const updateNotication = CatchAsyncError(async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        // Fetch the notification from the database using the notification ID from the request parameters
+        const notification = await NotificationModel.findById(req.params.id);
+
+        // If notification is not found, return a 404 error
+        if (!notification) {
+            return next(new ErrorHandler("Notification not found", 404));
+        } else {
+            // If the notification exists, set the status to 'read' if it's not already set
+            notification.status ? notification.status = 'read' : notification?.status;
+        }
+
+        // Save the updated notification status in the database
+        await notification.save();
+
+        // Fetch all notifications from the database, sorted by creation date in descending order (latest first)
+        const notifications = await NotificationModel.find().sort({
+            createdAt: -1,
+        });
+
+        // Respond with the success status and the updated list of notifications
+        res.status(201).json({
+            success: true,
+            notifications,
+        });
+
+    } catch (error: any) {
+        // If any error occurs, pass the error to the next middleware with a 500 status code
+        return next(new ErrorHandler(error.message, 500));
+    }
+});
