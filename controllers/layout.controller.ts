25822b3f749ec31679ce0a8eebcef0b71acf6632
Ajmal Faris <ajmalfaris.me@gmail.com>
2024-12-18 17:33:52 +0530

    Implement getLayoutByType functionality
    - Added API to fetch layout details based on the provided type.
    - Defined asynchronous function with error handling.
    - Returned a success response with layout data upon successful retrieval.

--- controllers/layout.controller.ts
+++ controllers/layout.controller.ts
@@ -159,3 +159,19 @@ export const editLayout = CatchAsyncError(async (req: Request, res: Response, ne
         return next(new ErrorHandler(error.message, 500));
     }
 });
+
+
+export const getLayoutByType = CatchAsyncError(async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const {type} = req.body;
+        const layout = await LayoutModel.findOne({type});
+        // Return success response
+        res.status(200).json({
+            success: true,
+            layout,
+        });
+
+    } catch (error:any) {
+        return next(new ErrorHandler(error.message, 500));
+    }
+})
