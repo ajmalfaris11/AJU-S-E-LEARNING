47d89da483d9b4858f641e6480dcf1014806258c
Ajmal Faris <ajmalfaris.me@gmail.com>
2024-12-18 17:34:13 +0530

    Define route for fetching layout by type
    - Added GET route "/get-layout" in layoutRoute.
    - Linked route to the getLayoutByType controller function.
    - Enabled fetching layout details based on type with appropriate response handling.

--- routes/layout.route.ts
+++ routes/layout.route.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import { authorizedRoles, isAuthenticated } from '../middleware/auth';
-import { createLayout, editLayout } from '../controllers/layout.controller';
+import { createLayout, editLayout, getLayoutByType } from '../controllers/layout.controller';
 const layoutRoute = express.Router();
 
 layoutRoute.post("/create-layout", isAuthenticated, authorizedRoles("admin"), createLayout);
 
 layoutRoute.put("/edit-layout", isAuthenticated, authorizedRoles("admin"), editLayout);
+
+layoutRoute.get("/get-layout", getLayoutByType);
+
 
 export default layoutRoute;
